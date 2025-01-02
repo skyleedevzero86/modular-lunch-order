8253594f673607c23aa4464745412a7c9fc25c5f
이경용 <sleekydz86@naver.com>
2025-01-03 00:42:26 +0900

    feat: add MyBatis and JPA management to infrastructure module

---
+++ lunch-infrastructure/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("kapt")
+}
+
+val queryDslVersion: String by extra
+
+dependencies {
+    implementation("org.mybatis:mybatis:3.5.18")
+    implementation("org.mybatis:mybatis-spring:3.0.4")
+
+    // QueryDSL 의존성 추가
+    implementation("com.querydsl:querydsl-jpa:5.0.0:jakarta")
+    kapt("com.querydsl:querydsl-apt:5.0.0:jakarta")
+    kapt("jakarta.annotation:jakarta.annotation-api")
+    kapt("jakarta.persistence:jakarta.persistence-api")
+
+}
+
+// Querydsl 설정 시작
+val generated = file("src/main/generated")
+
+tasks.withType<JavaCompile> {
+    options.generatedSourceOutputDirectory.set(generated)
+}
+
+sourceSets {
+    main {
+        kotlin.srcDirs += generated
+    }
+}
+
+tasks.named("clean") {
+    doLast {
+        generated.deleteRecursively()
+    }
+}
+
+kapt {
+    generateStubs = true
+}
