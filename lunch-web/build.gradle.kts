d591b7a43abf72ba0e507fecbe4c0ab5741b89ec
이경용 <sleekydz86@naver.com>
2025-01-03 00:39:05 +0900

    feat: add JSP and JSTL dependencies to Gradle

---
+++ lunch-web/build.gradle.kts
@@ -0,0 +1,6 @@
+dependencies {
+    // 스프링부트 3.0 이상
+    implementation("jakarta.servlet:jakarta.servlet-api:6.1.0") // 버전 6.0.0 이상으로 명시
+    implementation("jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.2")
+    implementation("org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.2")
+}
